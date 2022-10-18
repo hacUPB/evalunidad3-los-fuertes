5a4ba2d606c49dfbe04b513879430f3154fc2755
Phentecost12 <93055187+Phentecost12@users.noreply.github.com>
2022-10-18 17:58:30 -0500

    Se Reajusto El codigo

--- IStrategy.c
+++ IStrategy.c
@@ -6,27 +6,20 @@
 void __Algorithm_void(void *name)
 {
     char * result = malloc(sizeof(char[20]));
-    strcpy(result,"Sin Strat");
+    strcpy(result,"Sin Strat/n");
     printf("%s", result);
     free(result);
 }
 
 IStrategy *New_IStrategy()
 {
-    return (IStrategy*)malloc(sizeof(IStrategy));
+    IStrategy *I =(IStrategy*)malloc(sizeof(IStrategy));
+    I->Al = &__Algorithm_void;
+    return I;
+
 }
 
-void Contructor_IStrategy(IStrategy* this)
-{
-    this->Al = &__Algorithm_void;
-}
-
-void Destructor_IStrategy( IStrategy * this)
-{
-    
-}
-
-char * Funcion(IStrategy *this)
+char *Funcion(IStrategy *this)
 {
     this->Al(this);
 }
